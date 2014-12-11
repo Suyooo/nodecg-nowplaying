fd0900005a3f3cde62a1ef7dfc74dfab28de6b60
Suyooo <chrspangler@gmx.de>
2014-12-11 21:38:58 +0100

    Updated to latest NCG, added automatic display

--- dashboard/panel.js
+++ dashboard/panel.js
@@ -2,10 +2,11 @@
 	$('#musicshow').click(function() { nodecg.sendMessage('musicshow', ""); });
 	$('#musichide').click(function() { nodecg.sendMessage('musichide', ""); });
    
-    function prepMusic() {    
+    function prepMusic() {
       var tData = {
         "lastfm": $('#music-songsource').val(),
-        "msg": $('#music-message').val()
+        "msg": $('#music-message').val(),
+        "auto": $('#music-autoenabled').is(':checked') ? parseInt($('#music-autointerval').val()) : 0
       };  
       
       nodecg.sendMessage('musicupdate', JSON.stringify(tData));
--- view/nowplaying.js
+++ view/nowplaying.js
@@ -4,37 +4,49 @@ var msgtime = 5;		// how long sub message is displayed, in seconds
 var update = 1;			// how often the title is updated, in seconds
 var apikey = "";		// your last.fm API key (last.fm/api)
 
-var curline = -1;
-var songsource = "";
+var auto = 0;
 var showing = 0;
+var songsource = "";
+var lastsong = "";
+
+var curline = -1;
+var toggleto;
 	
 $(document).on('ncgReady', function () {
-	nodecg.listenFor('musicupdate', updateMessage);
+	nodecg.listenFor('musicupdate', updateMusic);
 	nodecg.listenFor('musicshow', showMusic);
 	nodecg.listenFor('musichide', hideMusic);
 	
 	function showMusic(data) {
 		updateSong();
-		showing = true;
+		if (showing==0) {
+			$('#musiccontainer').transition({
+				'left': '0'
+			}, 1000, 'ease-out');
+			showing=1;
+		}
 	}
 	function hideMusic(data) {
-		updateSong();
-		$('#musiccontainer').transition({
-			'left': '1280px'
-		}, 1000, 'ease-in');
-		showing = 0;
+		if (showing==1) {
+			$('#musiccontainer').transition({
+				'left': '1280px'
+			}, 1000, 'ease-in');
+			showing = 0;
+		}
 	}
 	
-	function updateMessage(data) {
+	function updateMusic(data) {
 		var msgParsed = JSON.parse(data);
 		
 		songsource = msgParsed.lastfm;
+		
+        auto = msgParsed.auto;
 		
         var msg = msgParsed.msg;
         if(msg) {
 			if (curline == -1) {
 				curline = 0;
-				setTimeout(toggleLines,titletime*1000);
+				toggleto=setTimeout(toggleLines,titletime*1000);
 			}
 			$('#musicmessage').html(msg);
 		} else {
@@ -42,6 +54,7 @@ $(document).on('ncgReady', function () {
 				if (curline==1) {
 					toggleLines();
 				}
+				clearTimeout(toggleto);
 				curline = -1;
 			}
 		}
@@ -49,55 +62,55 @@ $(document).on('ncgReady', function () {
 	
 	setInterval(updateSong,update*1000);
 	function updateSong() {
-		if (showing==0) return;
 		if (songsource=="") {
 			$.get("nowplaying.txt",function(data,status){
 				if (status=="success") {
-					$('#musictitle').text(data);
-					if (showing==1) {
-						$('#musiccontainer').transition({
-							'left': '0'
-						}, 1000, 'ease-out');
-						showing=2;
-					}
+					updateTitle(data);
 				}
 			});
 		} else {
 			$.getJSON("http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user="+songsource+"&api_key="+apikey+"&limit=2&format=json&callback=?", function(data) {
-				$('#musictitle').text(data.recenttracks.track[0].artist['#text'] + " - " + data.recenttracks.track[0].name);
-				if (showing==1) {
-					$('#musiccontainer').transition({
-						'left': '0'
-					}, 1000, 'ease-out');
-					showing=2;
-				}
+				 updateTitle(data.recenttracks.track[0].artist['#text'] + " - " + data.recenttracks.track[0].name);
 			});
 		}
     }
+	function updateTitle(song) {
+		$('#musictitle').text(song);
+		if (song != lastsong && auto != 0) {
+			if (curline==1) {
+				toggleLines();
+			}
+			showMusic();
+			clearTimeout(toggleto);
+			setTimeout(toggleLines,auto*600);
+			setTimeout(hideMusic,auto*1000);
+		}
+		lastsong = song;
+	}
 
 	function toggleLines() {
 		if (curline==0) {
 			$('#musictitle').transition({
 				'opacity': '0',
-				'margin-top': '0px'
+				'margin-top': '-4px'
 			}, 300, 'ease-out');
 			$('#musicmessage').delay(150).transition({
 				'opacity': '1',
-				'margin-top': '12px'
+				'margin-top': '8px'
 			}, 300, 'ease-out');
 			curline = 1;
-			setTimeout(toggleLines,msgtime*1000);
+			toggleto=setTimeout(toggleLines,msgtime*1000);
 		} else if (curline==1) {
 			$('#musictitle').delay(150).transition({
 				'opacity': '1',
-				'margin-top': '12px'
+				'margin-top': '8px'
 			}, 300, 'ease-out');
 			$('#musicmessage').transition({
 				'opacity': '0',
-				'margin-top': '24px'
+				'margin-top': '20px'
 			}, 300, 'ease-out');
 			curline = 0;
-			setTimeout(toggleLines,titletime*1000);
+			toggleto=setTimeout(toggleLines,titletime*1000);
 		}
 	}
 });

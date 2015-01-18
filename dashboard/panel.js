5bfafbe022dbf8cab2262ba9200121909d2925c0
Matthew McNamara <matt@mattmcn.com>
2015-01-18 17:58:08 +0000

    Bumped NodeCG dependency to 0.3, replaced messaged with synced variables for lastfm, message, auto-update, and new isShowing

--- dashboard/panel.js
+++ dashboard/panel.js
@@ -1,13 +1,35 @@
-    $('#musicupdate').click(function() { prepMusic(); });
-	$('#musicshow').click(function() { nodecg.sendMessage('musicshow', ""); });
-	$('#musichide').click(function() { nodecg.sendMessage('musichide', ""); });
-   
+    var showBtn = $('#musicshow');
+    var hideBtn = $('#musichide');
+    var updateBtn = $('#musicupdate');
+
+    updateBtn.click(function() { nodecg.variables.musicinfo = prepMusic(); });
+    showBtn.click(function() { nodecg.variables.isShowing = true; });
+    hideBtn.click(function() { nodecg.variables.isShowing = false; });
+
+    nodecg.declareSyncedVar({
+        name: 'isShowing',
+        initialValue: false,
+        setter: function (isShowing) {
+            showBtn.prop('disabled', isShowing);
+            hideBtn.prop('disabled', !isShowing);
+        }
+    });
+
+    nodecg.declareSyncedVar({
+        name: 'musicinfo',
+        initialValue: {},
+        setter: function (info) {
+            $('#music-songsource').val(info.lastfm);
+            $('#music-message').val(info.msg);
+            $('#music-autointerval').val(info.auto);
+            $('#music-autoenabled').prop('checked', info.auto !== 0);
+        }
+    });
+
     function prepMusic() {
-      var tData = {
-        "lastfm": $('#music-songsource').val(),
-        "msg": $('#music-message').val(),
-        "auto": $('#music-autoenabled').is(':checked') ? parseInt($('#music-autointerval').val()) : 0
-      };  
-      
-      nodecg.sendMessage('musicupdate', JSON.stringify(tData));
+        return {
+            lastfm: $('#music-songsource').val(),
+            msg: $('#music-message').val(),
+            auto: $('#music-autoenabled').is(':checked') ? parseInt($('#music-autointerval').val()) : 0
+        };
     }
